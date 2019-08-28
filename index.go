d34a6bd35df2006911edb1ec133bba17b795e56d
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-08-29 01:10:30 +0900

    feat: redirect using map

--- index.go
+++ index.go
@@ -6,11 +6,28 @@ import (
 	"strings"
 )
 
+var hostMap = map[string]string{
+	"heartbeat.haneru.dev": "hanerubeat.app",
+}
+
+func normalizeHost(host string) string {
+	if x := hostMap[host]; x != "" {
+		return x
+	}
+
+	if strings.HasPrefix(host, "www.") {
+		return strings.TrimPrefix(host, "www.")
+	}
+
+	return host
+}
+
 // Handler replies to the request
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if strings.HasPrefix(r.Host, "www.") {
-		host := strings.TrimPrefix(r.Host, "www.")
-		u := fmt.Sprintf("https://%s%s", host, r.URL.RequestURI())
+	newHost := normalizeHost(r.Host)
+
+	if newHost != r.Host {
+		u := fmt.Sprintf("https://%s%s", newHost, r.URL.RequestURI())
 
 		http.Redirect(w, r, u, http.StatusMovedPermanently)
 	} else {
